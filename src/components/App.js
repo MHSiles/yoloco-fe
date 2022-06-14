2a454aa29e22e1f655a4041d38edc73fa3adae08
Mauricio Hernandez <mhsiles1196@gmail.com>
2022-06-14 17:26:06 -0400

    Initial react commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div class="ui message">
+                <div class="header">
+                    Welcome to React!
+                </div>
+                <p>Sample text</p>
+            </div>
+        );
+      }
+}
+
+export default App;
