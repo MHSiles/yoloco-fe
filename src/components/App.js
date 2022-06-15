cfbd036b7b4b48fae81bd58476a257279b050ed4
Mauricio Hernandez <mhsiles1196@gmail.com>
2022-06-15 15:57:29 -0400

    Class to functional component & dynamic wallet adding

--- src/components/App.js
+++ src/components/App.js
@@ -12,43 +12,32 @@ const App = () => {
     )
 
 import React from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
 
-class App extends React.Component {
-    constructor(props){
-        super(props);
 
-        this.state = {
-            address : '',
-            listOfWallets: [],
+const App = () => {
+
+    const [address, setAddress] = useState('');
+    const [listOfWallets, setListOfWallets] = useState([]);
+
+    const handleTextChange = (e) => {
+        setAddress(e.target.value)
+    }
+    
+    const handleAdd = () => {
+        if (address !== ""){
+            setListOfWallets([...listOfWallets, address]);
+            setAddress('');
         }
-
-        this.handleTextChange = this.handleTextChange.bind(this);
-        this.handleAdd = this.handleAdd.bind(this);
     }
 
-    handleTextChange(e){
-        this.setState({address : e.target.value})
-    }
-    handleAdd = () => {
-        if (this.state.address != ""){
-            this.setState({listOfWallets: [...this.state.listOfWallets, this.state.address]})
-            //document.getElementById("text-input").reset();
-            this.setState({address: ""})
-            console.log(this.state.address)
-            console.log(this.state.listOfWallets)
-        }
-        
-        //reset text field
-
-    }
-
-    handleSubmit = () => {
-        console.log(this.state.address)
-        console.log(this.state.listOfWallets)
+    const handleSubmit = () => {
+        console.log(address)
+        console.log(listOfWallets)
 
         const postData = {
-            listOfWallets: this.state.listOfWallets
+            listOfWallets: listOfWallets
         }
         axios.post('https://jsonplaceholder.typicode.com/posts', {postData}).then(res => {
             console.log(res)
@@ -56,56 +45,44 @@ class App extends React.Component {
         }) 
     }
 
-    getSubmitState(){
-        return this.state.submitDisabled
-    }
-
-    render() {
-        return (
-            <div>
-                <div className="ui message">
-                    <h1 className="header" style={{marginLeft: "80px"}}>
-                    Crypto Portfolio Risk Calculator
-                    </h1>
-                </div>
-
-                <div className="ui form" style={{marginRight: "700px", marginLeft: "100px"}}>
-                    <div class="field">
-                        <label>Add a Wallet</label>
-                        <input type="text" placeholder="Wallet Address" onChange={this.handleTextChange}></input>
-                    </div>
-                    <div class="ui black bottom attached button" tabindex="0"  onClick={this.handleAdd}>Add</div>
-                
-
-                    <table class="ui table">
-                        <thead>
-                            <tr>
-                                <th>Wallets</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-
-                            {this.state.listOfWallets.map((wallet,i) => {
-                                return (
-                                    <tr key={"wallet-" + i}>
-                                        <input
-                                        // type="text"
-                                        value={wallet}
-                                    />
-                                    </tr>
-                                );
-                            })}
-
-                        </tbody>
-                    </table>
-                    <div class="ui green bottom attached button" tabindex="0" onClick={this.handleSubmit}>Submit</div>
-
-                </div>
-                
+    return (
+        <div>
+            <div className="ui message">
+                <h1 className="header" style={{marginLeft: "0px"}}>
+                Crypto Portfolio Risk Calculator
+                </h1>
             </div>
 
-        );
-      }
+            <div className="ui form" style={{marginRight: "40%", marginLeft: "10%"}}>
+                <div className="field">
+                    <label>Add a Wallet</label>
+                    <input type="text" value={address} placeholder="Wallet Address" onChange={handleTextChange}></input>
+                </div>
+                <div className="ui black bottom attached button" tabIndex="0"  onClick={handleAdd}>Add</div>
+            
+
+                <table className="ui table">
+                    <thead>
+                        <tr>
+                            <th>Wallets</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {listOfWallets.map((wallet,i) => {
+                            return (
+                                <tr key={"wallet-" + i}>
+                                    <td>{wallet}</td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+                <div className="ui green bottom attached button" tabIndex="0" onClick={handleSubmit}>Submit</div>
+            </div>
+        </div>
+
+    );
+
 }
 
 export default App;
